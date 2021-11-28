93795442d70a362a95ee5d5552a29915b7e4f5b0
NicolaL95 <nicola.lilla@gmail.com>
2021-11-28 19:33:53 +0100

    Added half footer structure

---
+++ src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="footer_container">
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <img src="../assets/footer-logo.png" alt="Logo" />
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
+            temporibus quod, sit corporis itaque necessitatibus. Perspiciatis
+            deleniti dolore laborum iure.
+          </p>
+          <button class="quote_button border-0 button_size">LEARN MRORE</button>
+        </div>
+        <div class="col-3">
+          <h5>LASTEST TWEETS</h5>
+          <div class="twitter_container">
+            <img src="../assets/logo_twit.png" alt="Logo" />
+            <div class="twitter_text">
+              <h6>ThemeFusiion</h6>
+              <p>@Theme_fusion</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Repellat fugiat dicta aliquam recusandae laboriosam esse
+                perspiciatis beatae!
+              </p>
+              <img src="../assets/promo.jpeg" alt="" />
+              <div class="icon_share">
+                <div class="icon"></div>
+                <div class="date"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+#footer_container {
+  background-color: #1e1e1e;
+}
+</style>
